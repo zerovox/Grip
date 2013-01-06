31d4f3baa59decfe98fd9cfce69bf7422c6099cf
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2013-01-06 22:41:36 +0000

    Allow adding new test using alertify prompts

--- js/views/testList.js
+++ js/views/testList.js
@@ -1,12 +1,14 @@
 define([
     'backbone',
     'mustache',
-    'libs/text!templates/testList.html'
-], function (Backbone, Mustache, TestListTemplate) {
+    'libs/text!templates/testList.html',
+    'alertify'
+], function (Backbone, Mustache, TestListTemplate, alertify) {
 
     return Backbone.View.extend({
         el         : '.testTableBody',
-        initialize : function (testCollection) {
+        initialize : function () {
+            this.bind();
         },
         set        : function (testCollection) {
             this.tests = testCollection;
@@ -14,11 +16,30 @@ define([
         },
         render     : function () {
             var tests = this.tests.toJSON()
-            _.each(tests, function(test, index){
+            _.each(tests, function (test, index) {
                 test.index = index;
             })
             var html = Mustache.render(TestListTemplate, {tests : tests});
             this.$el.html(html);
+
+        },
+        bind     : function () {
+            var that = this;
+            $('#addTestCase').on("click", function () {
+                alertify.prompt("Please enter the inputs as a comma separated list with strings enclosed by quotation marks.", function (e, inputs) {
+                    if (e) {
+                        alertify.prompt("Please enter the desired output for this test with strings enclosed by quotation marks.", function (e, output) {
+                            if (e) {
+                                that.tests.add({inputs:JSON.parse('['+inputs+']'), output:JSON.parse(output), status:"Not yet run"})
+                                that.render();
+
+                                //TODO: Handle JSON parse fails with log messages
+                            }
+                        })
+                    }
+                })
+            });
+
         }
     });
 

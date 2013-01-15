8a7b4efa64f9caaa676b8cee7f2b9a80561b1514
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2013-01-15 15:27:13 +0000

    add alertify alerts based on test pass or fail

--- js/models/TestModel.js
+++ js/models/TestModel.js
@@ -1,6 +1,7 @@
 define([
-    'backbone'
-], function (Backbone) {
+    'backbone',
+    'alertify'
+], function (Backbone, alertify) {
 
     return Backbone.Model.extend({
         complete : function(result){
@@ -8,11 +9,18 @@ define([
 
             if(this.get("output") === result){
                 this.set("passed", true)
+                alertify.success("Test passed!")
             } else {
                 this.set("passed", false)
+                alertify.error("Test failed!")
             }
 
             this.set("lastResult", result)
+        },
+        fail : function(failMsg){
+            this.set("finished", true)
+            this.set("passed", false)
+            this.set("lastResult", "Error: " + failMsg)
         }
     });
 

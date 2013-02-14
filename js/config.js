6a816af5ad521c7b9767ead19a95d3fad4fab5d1
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2013-02-14 15:59:26 +0000

    Move config to seperate file, alows it to be reused for tests

---
+++ js/config.js
@@ -0,0 +1,25 @@
+var require = {
+    baseUrl : "/js",
+    paths   : {
+        underscore       : 'libs/lodash.min',
+        jquery           : 'libs/jquery.min',
+        backbone         : 'libs/backbone.min',
+        templates        : '../templates',
+        mustache         : "libs/mustache",
+        fabric           : "libs/fabric.wrap",
+        primitives       : "data/prim.wrap",
+        alertify         : "libs/alertify.min",
+        foundation       : "libs/foundation.min",
+        'foundation.app' : "libs/foundation.app",
+        modernizr        : "libs/foundation.modernizr"
+    },
+    shim    : {
+        'backbone'       : {
+            deps    : ['underscore', 'jquery'],
+            exports : 'Backbone'
+        },
+        'foundation.app' : {
+            deps : ['modernizr', 'jquery', 'foundation']
+        }
+
+    }};

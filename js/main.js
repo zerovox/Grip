d51f99732e8072b3f5dceb042780718ec4f5db26
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2013-02-14 16:19:01 +0000

    Don't need to register an App module, move loading logic into main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-require(['app', 'foundation.app'], function (App) {
-    App.initialize();
+require([
+    'underscore',
+    'backbone',
+    'router',
+    'views/Main',
+    'foundation.app'],
+    function (_, Backbone, Router, MainView, FoundationApp) {
+
+    _.extend(Backbone.View.prototype, {
+        hide    : function () {
+            this.$el.hide()
+        }, show : function () {
+            this.$el.show()
+        }
+    })
+
+    var mainView = new MainView()
+    mainView.render()
+
+    Router.initialize();
 });

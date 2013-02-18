9a0a3c5086ba7a25d1ba577b1311abbfafc0c87e
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2013-02-18 01:06:22 +0000

    Fix typo

--- js/models/FunctionModel.js
+++ js/models/FunctionModel.js
@@ -5,7 +5,7 @@ define([
 
     return Backbone.Model.extend({
         initialize : function (fName, extra) {
-            //Extra holds a pointer to the collection it is being initilized in, could be used as a way to hold primitives in one place
+            //Extra holds a pointer to the collection it is being initialized in, could be used as a way to hold primitives in one place
             var func = _.find(primitives, function (prim) {return prim.name === fName})
             this.set({func : func, group : func.group})
         }});

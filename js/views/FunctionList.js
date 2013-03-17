89bd10a08ea3702e3be1225fdfa1693bffe8d85f
Timothy Slatcher <timothy.slatcher@magd.ox.ac.uk>
2013-03-17 20:10:03 +0000

    Hack to allow FunctionList to correctly register it's onClick listeners

--- js/views/FunctionList.js
+++ js/views/FunctionList.js
@@ -18,22 +18,26 @@ define([
             this.list = functionsCollection
             this.locals = editorCollection
             this.groupView = new GroupView(this.groups)
-            this.render(_.first(this.groups).name)
+            this.name = _.first(this.groups).name
+            this.render()
+
+            //Hack: This lets us wait till the DOM has finished loading before re-drawing the canvas.
+            //The images will be correct without this line, but the onClick handlers might not be calibrated to the page offset correctly.
+            //We only need to do this for the initial draw when the browser is busy.
+            setTimeout(this.listView.show, 0)
         },
         makeActive     : function (name) {
             _.find(this.groups,function (obj) {return obj.active}).active = false;
             _.find(this.groups,function (obj) {return name === obj.name}).active = true;
             this.groupView = new GroupView(this.groups)
-            this.render(name)
+            this.name = name
+            this.render()
         },
-        render         : function (name) {
-            if (name === "LocalFunctions")
+        render         : function () {
+            if (this.name === "LocalFunctions")
                 this.listView.set(this.locals)
             else
-                this.listView.set(this.list.where({group : name}))
-
-            this.listView.show()
-
+                this.listView.set(this.list.where({group : this.name}))
         },
         removeChildren : function () {
             this.groupView.remove()
